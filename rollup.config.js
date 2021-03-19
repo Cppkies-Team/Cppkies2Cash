ec29126ae7dfae03310013282cc3fb93171379d8
Lubomir <lubomir.yudenko@gmail.com>
2021-03-19 18:58:29 +0200

    Add initial files

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import analyze from "rollup-plugin-analyzer"
+import { terser } from "rollup-plugin-terser"
+import typescript from "rollup-plugin-typescript2"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json"
+import path from "path"
+import fs from "fs"
+
+const production = process.env.NODE_ENV === "production"
+const esmDebugging = process.env.CPPKIES_ESM_DEBUGGING === "yes"
+
+const plugins = [
+	typescript({
+		tsconfig: production ? "./tsconfig.json" : "./tsconfig.dev.json",
+		objectHashIgnoreUnknownHack: true,
+	}),
+	json(),
+	resolve(),
+	commonjs(),
+	analyze({
+		summaryOnly: true,
+	}),
+	production ? terser() : undefined,
+]
+
+function getFilesRecursive(dir) {
+	const files = []
+	fs.readdirSync(dir).forEach(File => {
+		const absolute = path.join(dir, File)
+		if (fs.statSync(absolute).isDirectory())
+			files.push(...getFilesRecursive(absolute))
+		else files.push(absolute)
+	})
+	return files
+}
+
+//import banner from "rollup-plugin-banner"
+export default [
+	...(!esmDebugging
+		? [
+				{
+					input: ["./src/index.ts"],
+					output: {
+						name: "Cppkies2Cash",
+						file: "./dist/index.js",
+						format: "umd",
+						sourcemap: true,
+					},
+					plugins,
+				},
+		  ]
+		: []),
+	...(production || esmDebugging
+		? [
+				{
+					input: getFilesRecursive("./src").map(val => `./${val}`),
+					output: {
+						dir: "./dist/esm",
+						format: "esm",
+					},
+					plugins,
+				},
+		  ]
+		: []),
+]
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Test")
+
+export interface Test {
+	testThing: "thisIsForTheDTSGenerator"
+}
